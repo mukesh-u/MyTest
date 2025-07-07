c3c057072bef1ee40b3af6fa30e9734c65bf91a5
mukesh.u <mukesh.u@media.net>
2025-07-07 16:47:53 +0530

    modified dummy file

--- MyProjects/tests/dummyTest.spec.js
+++ MyProjects/tests/dummyTest.spec.js
@@ -3,4 +3,5 @@ import { test } from "playwright/test";
 test("Testing dummy website for CI/CD", async ({ page }) => {
   await page.goto("http://www.example.com/");
   await page.getByText("Example Domain");
+  await page.close();
 });
