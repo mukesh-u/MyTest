327bef617d4101d0893f28d4eea551903f5b3e8c
mukesh.u <mukesh.u@media.net>
2025-07-07 16:35:04 +0530

    Testing dummy file for CI/CD

---
+++ MyProjects/tests/dummyTest.spec.js
@@ -0,0 +1,6 @@
+import { test } from "playwright/test";
+
+test("Testing dummy website for CI/CD", async ({ page }) => {
+  await page.goto("http://www.example.com/");
+  await page.getByText("Example Domain");
+});
